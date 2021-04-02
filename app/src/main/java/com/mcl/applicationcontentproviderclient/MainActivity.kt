8c48d260988241d2e5f0b8acb80f592df16eb884
MarceloCLopes <marc01mcl@gmail.com>
2021-04-02 12:25:00 -0300

    updated main activity method get content provider

--- app/src/main/java/com/mcl/applicationcontentproviderclient/MainActivity.kt
+++ app/src/main/java/com/mcl/applicationcontentproviderclient/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.mcl.applicationcontentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +20,22 @@ class MainActivity : AppCompatActivity() {
 
         noteRefreshButton = findViewById(R.id.client_button_refresh)
         notesRecycler = findViewById(R.id.client_list)
+        getContentProvider()
 
-        noteRefreshButton.setOnClickListener {  }
+        noteRefreshButton.setOnClickListener { getContentProvider() }
+    }
 
-
+    private fun getContentProvider(){
+        try {
+            val url = "content://com.mcl.applicationcontentprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? = contentResolver.query(data, null, null, null, "title")
+            notesRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        } catch (ex: Exception){
+            ex.printStackTrace()
+        }
     }
 }
